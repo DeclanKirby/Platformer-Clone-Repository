c75eba2fe371eed56f69ed2f0f2e176bfbcec34d
aidank4 <148916563+aidank4@users.noreply.github.com>
2023-10-25 19:35:12 -0700

    Added Comments for health and UI

--- Platformer Clone/Assets/Scripts/PlayerController.cs
+++ Platformer Clone/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ using UnityEngine.SceneManagement;
 public class PlayerMove : MonoBehaviour
 {
     public int bulletSpeed = 5;
+
+    public int health = 99;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +18,28 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        /*if (health <= 0)
+        {
+            Debug.Log("Game Over");
+            SceneManager.LoadScene(2);
+        }
+        */
     }
 
     private void Shoot()
     {
        
     }
+    /*private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "BasicEnemy")
+        {
+            health += -15;
+        }
+        if (other.gameObject.tag == "AdvancedEnemy")
+        {
+            health += -35;
+        }
+    }
+    */
 }
---
+++ Platformer Clone/Assets/Scripts/UIManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+//Kelly, Aidan
+//10/25/23
+//Controls UI
+
+public class UIManager : MonoBehaviour
+{
+    public TMP_Text healthText;
+    //public PlayerController playerController;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //healthText.text = "Health: " + playerController.health; yet to be added, int for the players changing health
+    }
+}
