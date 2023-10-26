60e9cd8d5434e5405336cfa590c3644011ebca0e
MaxHernandez03 <maximus957282@my.yosemite.edu>
2023-10-25 20:50:45 -0700

    Added left and right movement to player controller.
    I also added a jump function. I noticed that I could not attach the player controller script to the object though.

--- Platformer Clone/Assets/Scripts/PlayerController.cs
+++ Platformer Clone/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,20 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayerMove : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    //These are movement variables
+    public float speed = 10f;
+    public float jumpForce = 7f;
+    private Rigidbody rigidbodyRef;
+
     public int bulletSpeed = 5;
 
     public int health = 99;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbodyRef = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -24,6 +29,33 @@ public class PlayerMove : MonoBehaviour
             SceneManager.LoadScene(2);
         }
         */
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            PlayerJump();
+        }
+    }
+
+    private void PlayerJump()
+    {
+        RaycastHit hit;
+
+        //Adjust the value based on size of player character
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 0.8f))
+        {
+            
+            rigidbodyRef.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 
     private void Shoot()
