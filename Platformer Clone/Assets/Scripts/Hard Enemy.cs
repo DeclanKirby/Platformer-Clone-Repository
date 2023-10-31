1b7c7b7126c5f27828c02df4b0670a2480dcadff
DeclanKirby <declanpkirby@gmail.com>
2023-10-31 15:44:35 -0700

    End of class

--- Platformer Clone/Assets/Scripts/Hard Enemy.cs
+++ Platformer Clone/Assets/Scripts/Hard Enemy.cs
@@ -6,15 +6,42 @@ public class HardEnemy : MonoBehaviour
 {
     public float enemyHealth = 10;
     public float speed = 7;
+    public float detectionRange = 6;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(enemyHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    private void CheckForPlayer()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Vector3.right, out hit, detectionRange))
+        {
+            if (hit.collider.tag == "Player")
+            {
+                transform.position += Vector3.right * speed * Time.deltaTime;
+            }
+        }
+
+        if (Physics.Raycast(transform.position, Vector3.left, out hit, detectionRange))
+        {
+            if (hit.collider.tag == "Player")
+            {
+                transform.position += Vector3.left * speed * Time.deltaTime;
+            }
+        }
+
+
+
     }
 }
